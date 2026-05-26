e3d5ab651025d75a0c044e2e62710a89677b9266
kimhg990212 <kimhg990212@gmail.com>
2026-05-26 11:47:08 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,3 +30,33 @@ if __name__ == "__main__":
 
 # FR-05기능 추가 라우터
 app.include_router(text_logo.router, prefix="/text-logo")
+
+# FR-08기능 추가 라우터
+from routes.generate_route import (
+    router as generate_router
+)
+
+from middleware.error_middleware import (
+    global_exception_handler
+)
+app.include_router(generate_router)
+
+app.add_exception_handler(
+    Exception,
+    global_exception_handler
+)
+
+@app.get("/")
+def root():
+
+    return {
+        "message": "Server Running"
+    }
+)
+#FR-09기능 추가 라우터 
+from routes.search_route import (
+    router as search_router
+)
+
+app.include_router(
+    search_router
